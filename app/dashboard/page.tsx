46273c7d0132acd8abc83ee4911bb5efe0a0d120
emre1n <emre1n@protonmail.com>
2023-11-19 18:33:31 +0300

    add prisma and schema on sqlite db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,15 @@
+// import db from '@/database/db';
+
+// To seed the database
+// async function createCustomer() {
+//   customers.forEach(async (customerData) => {
+//     await db.customer.create({
+//       data: customerData,
+//     });
+//   });
+// }
+// createCustomer();
+
 export default function Page() {
   return <p>Dashboard Page</p>;
 }
